bd552355dee6ac5947205c35161eb9191c88fd38
hanzhihua <hanzhihua@bilibili.com>
2023-11-16 10:52:28 +0800

    使用'cd'参数替换'c'

--- cmd/yajs.go
+++ cmd/yajs.go
@@ -18,7 +18,7 @@ func init() {
 	flag.BoolVar(&showHelp, "help", false, "Show help")
 	flag.IntVar(&utils.Port,"p", 2222, "Port")
 	defaultConfigDir := fmt.Sprintf("%s%s", utils.ProcUser.HomeDir, "/github.com/hanzhihua/yajs/")
-	flag.StringVar(&utils.ConfigDir,"c",defaultConfigDir,"Config Directory")
+	flag.StringVar(&utils.ConfigDir,"cd",defaultConfigDir,"Config Directory")
 	flag.IntVar(&utils.SshIdleTimeout,"ssh.idletimeout",120,"Ssh idletimeout")
 	flag.Parse()
 }
